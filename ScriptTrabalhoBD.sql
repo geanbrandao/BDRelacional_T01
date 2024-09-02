c9c27a4d1e6645b7fd86306436a3745580aebf62
GeanCarlosBr <geanbrandao96@gmail.com>
2024-09-02 09:10:00 -0300

    Update ScriptTrabalhoBD.sql

--- ScriptTrabalhoBD.sql
+++ ScriptTrabalhoBD.sql
@@ -62,3 +62,30 @@ CREATE TABLE IF NOT EXISTS Estoque (
     quantidade INT NOT NULL,
     FOREIGN KEY (idLivro) REFERENCES Livro(idLivro)
 );
+
+-- CONSULTAS 
+
+-- Query para contar o total de livros cadastrados
+SELECT COUNT(*) AS TotalOfBooks FROM Livro;
+
+-- Query para listar os nomes dos clientes em ordem crescente
+SELECT nome FROM Cliente ORDER BY nome ASC;
+
+-- Query para listar as editoras e seus respectivos livros em ordem decrescente pelo nome da editora
+SELECT e.nome AS Editora, l.titulo AS Livro
+FROM Editora e
+JOIN Livro l ON e.idEditora = l.idEditora
+ORDER BY e.nome DESC;
+
+-- query para listar o nome das editoras e a média de preço dos livros
+SELECT e.nome AS Editora, AVG(l.preco) AS MediaPreco
+FROM Editora e
+JOIN Livro l ON e.idEditora = l.idEditora
+GROUP BY e.nome;
+
+-- queryy para listar o nome dos clientes e a quantidade de livros comprados
+SELECT c.nome AS Cliente, COUNT(ip.idLivro) AS LivrosComprados
+FROM Cliente c
+JOIN Pedido p ON c.idCliente = p.idCliente
+JOIN ItemPedido ip ON p.idPedido = ip.idPedido
+GROUP BY c.nome;
